80e91354cc77299eaaf4feae7c25ebf9fbb88523
cupreme19 <csupreme19@gmail.com>
2022-02-13 01:53:59 +0900

    공통 모델 정의

---
+++ src/main/java/com/api/model/GenericMessage.java
@@ -0,0 +1,14 @@
+package com.api.model;
+
+
+/**
+ * API 서버 표준 응답 메시지
+ * 
+ * @author csupreme19
+ * @since 2022.02.12
+ */
+public class GenericMessage {
+	String code;
+	String message;
+	Object result;
+}
