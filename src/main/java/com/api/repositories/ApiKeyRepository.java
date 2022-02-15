8f792b75bcee1690de36a1c522748b0f09900286
cupreme19 <csupreme19@gmail.com>
2022-02-15 20:42:32 +0900

    스프링 data rest 제거

--- src/main/java/com/api/repositories/ApiKeyRepository.java
+++ src/main/java/com/api/repositories/ApiKeyRepository.java
@@ -1,17 +1,12 @@
 package com.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
 import com.api.model.db.ApiKeyInfo;
 
-@RepositoryRestResource
+@Repository
 public interface ApiKeyRepository extends JpaRepository<ApiKeyInfo, Long> {
-
-	@RestResource
 	ApiKeyInfo findOneById(Long id);
-	
-	@RestResource
 	ApiKeyInfo findOneByCompanyId(Long companyId);
 }
--- src/main/java/com/api/repositories/CompanyRepository.java
+++ src/main/java/com/api/repositories/CompanyRepository.java
@@ -1,17 +1,12 @@
 package com.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
 import com.api.model.db.CompanyInfo;
 
-@RepositoryRestResource
+@Repository
 public interface CompanyRepository extends JpaRepository<CompanyInfo, Long> {
-
-	@RestResource
 	CompanyInfo findOneById(Long id);
-	
-	@RestResource
 	CompanyInfo findOneByName(String name);
 }
